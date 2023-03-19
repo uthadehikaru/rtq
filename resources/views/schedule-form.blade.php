4c6d29e218d51ac9a9876372df9a09cc32615de3
zuhriutama <zuhriutama@gmail.com>
2023-03-19 20:09:35 +0700

    change image size for teacher present

--- resources/views/schedule-form.blade.php
+++ resources/views/schedule-form.blade.php
@@ -42,7 +42,7 @@
         <p id="locationData"></p>
     </div>
     <div class="col-md-6">
-        <video id="webcam" autoplay playsinline width="640" height="480"></video>
+        <video id="webcam" autoplay playsinline width="300" height="400"></video>
         <img id="photo" download="selfie.png" class="d-none" />
         <canvas id="canvas" class="d-none"></canvas>
         <audio id="snapSound" src="{{ asset('assets/snap.wav') }}" preload = "auto"></audio>
