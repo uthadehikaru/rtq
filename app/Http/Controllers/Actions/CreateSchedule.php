1e33a1445e9a68e5f891c3477c1b9deec979c310
zuhriutama <zuhriutama@gmail.com>
2023-04-04 16:25:53 +0700

    bug fix when failed create schedule image

--- app/Http/Controllers/Actions/CreateSchedule.php
+++ app/Http/Controllers/Actions/CreateSchedule.php
@@ -36,32 +36,32 @@ class CreateSchedule extends Controller
             return response(['error' => 'Absen halaqoh '.$batch->name.' hanya bisa dilakukan pada jam '.$batch->start_time->format('H:i')]);
         }
 
-        $file = 'jadwal/'.Auth::user()->name.' '.$batch->name.' '.Carbon::now()->format('d-M-Y H-i').'.jpg';
-        Storage::disk('public')->put($file, file_get_contents($data['photo']));
-        $image = Image::make(Storage::disk('public')->get($file));
-        $image->rectangle(5, 5, 250, 150, function ($draw) {
-            $draw->background('rgba(255, 255, 255, 0.5)');
-            $draw->border(2, '#000');
-        });
-        $y = 10;
-        $texts = $request->only(['lat', 'long']);
-        $texts['Tanggal, jam'] = Carbon::now()->format('d M Y, H:i');
-        $texts['Nama'] = Auth::user()->name;
-        $texts['Halaqoh'] = $batch->name;
-        $texts['Pengganti'] = $data['badal'] ? 'Ya' : 'Tidak';
-        $texts['posisi'] = $this->calculateDistance($texts).' meter';
-        foreach ($texts as $key => $text) {
-            $image->text($key.' : '.$text, 10, $y, function ($font) {
-                $font->file(public_path('assets/fonts/Roboto-Regular.ttf'));
-                $font->size(14);
-                $font->align('left');
-                $font->valign('top');
-            });
-            $y += 20;
-        }
-        $image->save(storage_path('app/public/'.$file));
-
         try {
+            $file = 'jadwal/'.Auth::user()->name.' '.$batch->name.' '.Carbon::now()->format('d-M-Y H-i').'.jpg';
+            Storage::disk('public')->put($file, file_get_contents($data['photo']));
+            $image = Image::make(Storage::disk('public')->get($file));
+            $image->rectangle(5, 5, 250, 150, function ($draw) {
+                $draw->background('rgba(255, 255, 255, 0.5)');
+                $draw->border(2, '#000');
+            });
+            $y = 10;
+            $texts = $request->only(['lat', 'long']);
+            $texts['Tanggal, jam'] = Carbon::now()->format('d M Y, H:i');
+            $texts['Nama'] = Auth::user()->name;
+            $texts['Halaqoh'] = $batch->name;
+            $texts['Pengganti'] = $data['badal'] ? 'Ya' : 'Tidak';
+            $texts['posisi'] = $this->calculateDistance($texts).' meter';
+            foreach ($texts as $key => $text) {
+                $image->text($key.' : '.$text, 10, $y, function ($font) {
+                    $font->file(public_path('assets/fonts/Roboto-Regular.ttf'));
+                    $font->size(14);
+                    $font->align('left');
+                    $font->valign('top');
+                });
+                $y += 20;
+            }
+            $image->save(storage_path('app/public/'.$file));
+            
             $data = [
                 'batch_id' => $request->get('batch_id'),
                 'is_badal' => $request->get('badal'),
