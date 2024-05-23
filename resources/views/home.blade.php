bc44b50ca226c62752114dc098b8fc9f8d9985b6
zuhriutama <zuhriutama@gmail.com>
2024-05-23 09:54:10 +0700

    minor bug fix youtube width on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
 			</div>
 
 			<div class="mx-auto xl:mx-0">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/W-vb62xcWqs?si=fbhkfromDhCPCJuP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			<iframe width="100%" height="315" src="https://www.youtube.com/embed/W-vb62xcWqs?si=fbhkfromDhCPCJuP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 			</div>
 
 		</header>
